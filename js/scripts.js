a1aa106fb58d8670ee9c33a713732cbc9bc0fb43
Phumlani123 <p.gwamanda@gmail.com>
2019-04-12 14:41:16 +0200

    added scripts fine tuning styles

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,83 @@ $(document).ready(function() {
 
 
 baguetteBox.run('.compact-gallery', {animation:'slideIn'});
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+// counter
+
+var a = 0;
+$(window).scroll(function() {
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
+
+                });
+        });
+        a = 1;
+    }
+
+});
+
+$(document).on('click', '.nav-link', function() {
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+});
+
+// scrolling ative class
+$(document).ready(function () {
+    $(window).scroll(function () {
+
+        var y = $(this).scrollTop();
+
+        $('.link').each(function (event) {
+            if (y >= $($(this).attr('href')).offset().top - 100) {
+                $('.link').not(this).removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+    });
+});
